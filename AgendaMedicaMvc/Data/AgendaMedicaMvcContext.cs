9b906d6582a7f2aa6a7ced62cfa617fb403815f2
Rafael Kowalski <rafaelmk06@gmail.com>
2022-07-04 22:21:59 -0300

    Criando um CRUD Scaffolding da classe Medicos

---
+++ AgendaMedicaMvc/Data/AgendaMedicaMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AgendaMedicaMvc.Models;
+
+namespace AgendaMedicaMvc.Data
+{
+    public class AgendaMedicaMvcContext : DbContext
+    {
+        public AgendaMedicaMvcContext (DbContextOptions<AgendaMedicaMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AgendaMedicaMvc.Models.Medico> Medico { get; set; }
+    }
+}
