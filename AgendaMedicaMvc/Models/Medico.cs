1ac1ba4f663545c25d4d221fc222cccbc2c4ed4e
Rafael Kowalski <rafaelmk06@gmail.com>
2022-07-05 22:18:36 -0300

    Implementando as validações ao Nome e alterando o tema dos botões

--- AgendaMedicaMvc/Models/Medico.cs
+++ AgendaMedicaMvc/Models/Medico.cs
@@ -1,9 +1,14 @@
-﻿namespace AgendaMedicaMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaMedicaMvc.Models
 {
     public class Medico
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "campo obrigatório")]
+        [Display(Name = "Nome")]
         public string Name { get; set; }
+        [Display(Name = "Especialização")]
         public string Specialization { get; set; }
 
         public Medico()
