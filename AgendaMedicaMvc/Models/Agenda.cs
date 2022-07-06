50a7bc3c2fe078041ae7cc4d70c1de3f5b6b6ad5
Rafael Kowalski <rafaelmk06@gmail.com>
2022-07-05 21:50:57 -0300

    Criado a classe Agenda e Enum StatusDaAgenda

---
+++ AgendaMedicaMvc/Models/Agenda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AgendaMedicaMvc.Models.Enums;
+
+namespace AgendaMedicaMvc.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Medico Medico { get; set; }
+        public Paciente Paciente { get; set; }
+        public StatusDaAgenda StatusDaAgenda { get; set; }
+
+        public Agenda()
+        {
+        }
+        public Agenda(int id, DateTime date, Medico medico, Paciente paciente, StatusDaAgenda statusDaAgenda)
+        {
+            Id = id;
+            Date = date;
+            Medico = medico;
+            Paciente = paciente;
+            StatusDaAgenda = statusDaAgenda;
+        }
+    }
+}
---
+++ AgendaMedicaMvc/Models/Enums/StatusDaAgenda.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaMedicaMvc.Models.Enums
+{
+    public enum StatusDaAgenda : int
+    {
+        AgendaConfirmada = 1,
+        AgendaConcluida = 2,
+        AgendaCancelada = 3,
+
+    }
+}
